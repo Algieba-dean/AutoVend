184f11ed019047663db1b280dc038a4aeef2eae2
Xavier <tangzhengwei97@qq.com>
2025-05-13 08:21:29 +0800

    save tmp changes
    save tmp changes

--- ModelQuery/QueryService.py
+++ ModelQuery/QueryService.py
@@ -1,9 +1,19 @@
+import tomllib as tomli
+from pprint import pp
+
 class CarModelQuery:
     def query_car_model(self, dict_query: dict) -> str:
         dict_query = {
-            "size": "Small",
-            "vehicle_category_bottom": "Micro Sedan",
+            "prize": "Small"
         }
+
+        with open("./ModelQuery/Labels.toml", "rb") as f:
+            Labels_dict = tomli.load(f)
+
+        pp(Labels_dict["prize"])
 
         model_names = ["Volvo-XC60 Recharge T8 eAWD R-Design", "Volvo-XC40 Recharge T8 eAWD R-Design", "Volvo-XC90 Recharge T8 eAWD R-Design"]
         return model_names
+
+c=CarModelQuery()
+c.query_car_model({})
