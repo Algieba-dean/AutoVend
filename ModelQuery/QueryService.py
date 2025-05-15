993129c50dd4509fdafa12c776ca14f66feb682b
Xavier Tang （唐正为） <xavier.tang@cerence.com>
2025-05-15 09:43:29 +0800

    Update query label file and label tree

--- ModelQuery/QueryService.py
+++ ModelQuery/QueryService.py
@@ -1,4 +1,5 @@
 import tomllib as tomli
+import json as json
 from pprint import pp
 
 class CarModelQuery:
@@ -6,11 +7,15 @@ class CarModelQuery:
         dict_query = {
             "prize": "Small"
         }
-
+        with open('./ModelQuery/LabelsTree.json', 'r') as file_query_tree:
+            query_tree = json.load(file_query_tree)
         with open("./ModelQuery/Labels.toml", "rb") as f:
             Labels_dict = tomli.load(f)
 
-        pp(Labels_dict["prize"])
+        #pp(query_tree)
+        for precise_lable,value in query_tree["precise needs"].items():
+            pp(precise_lable)
+            pp(value)
 
         model_names = ["Volvo-XC60 Recharge T8 eAWD R-Design", "Volvo-XC40 Recharge T8 eAWD R-Design", "Volvo-XC90 Recharge T8 eAWD R-Design"]
         return model_names
